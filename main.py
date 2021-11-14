921595a76f70f7631051ae917a832530dee7ebad
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2021-11-14 20:10:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import json
 import random 
 from replit import db
-from keep_alive import keep_alive
+
 
 client= discord.Client()
 
@@ -16,6 +16,8 @@ healer_a=["Great work! keep it up buddy", "Keep going buddy","Stay healthy buddy
 
 healer_b=["Don't worry buddy","Just work hard buddy","Don't get stressed...type ""/heal"" to get some motivation",]
 encourage=["Way to go!","You are amazing","Sky is not your limit"]
+
+start=["Hi","Hello","Hey","Hola"]
 
 if "responding" not in db.keys():
   db["responding"]=True
@@ -53,53 +55,45 @@ async def on_message(message):
 
   msg=message.content 
 
-  if db["responding"]:
-   options=encourage
-   if "encouragements" in db.keys():
-    options.extend(db["encouragements"])
+  if any(word in msg for word in start):
+    await message.channel.send("Hi. Hope you are fine")
+
+  
+  options=encourage
+  if "encouragements" in db.keys():
+   options.extend(db["encouragements"])
    
-   if message.content.startswith("/heal"):
-    quote=get_quote()
-    await message.channel.send(quote)
+  if message.content.startswith("/heal"):
+   quote=get_quote()
+   await message.channel.send(quote)
 
-   if msg.startswith("/appreciate"):
-     await message.channel.send(random.choice(options))
+  if msg.startswith("/appreciate"):
+    await message.channel.send(random.choice(options))
 
-   if msg.startswith("/add"):
-     encouraging_message=msg.split("/add ",1)[1]
-     update_encouragements(encouraging_message)
-     await message.channel.send("Appreciation added")
+  if msg.startswith("/add"):
+    encouraging_message=msg.split("/add ",1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send("Appreciation added")
 
-   if msg.startswith("/remove"):
-     encouragements=[]
-     if "encouragements" in db.keys():
-       index=int(msg.split("/remove",1)[1])
-       delete_encouragements(index)
-       encouragements=db["ëncouragements"]
-     await message.channel.send(encouragements)
+  if msg.startswith("/remove"):
+    encouragements=[]
+    if "encouragements" in db.keys():
+      index=int(msg.split("/remove",1)[1])
+      delete_encouragements(index)
+      encouragements=db["ëncouragements"]
+    await message.channel.send(encouragements)
 
-   if msg.startswith("/list"):
-     encouragements=[]
-     if "encouragements" in db.keys():
-       encouragements=db["encouragements"]
-     await message.channel.send(encouragements)
+  if msg.startswith("/list"):
+    encouragements=[]
+    if "encouragements" in db.keys():
+      encouragements=db["encouragements"]
+    await message.channel.send(encouragements)
 
-   if any(word in msg for word in bad_words):
-     await message.channel.send(random.choice(healer_b))
+  if any(word in msg for word in bad_words):
+    await message.channel.send(random.choice(healer_b))
     
-   if any(word in msg for word in good_words):
-     await message.channel.send(random.choice(healer_a))
+  if any(word in msg for word in good_words):
+    await message.channel.send(random.choice(healer_a))
 
-  if msg.startswith("/responding"):
-    value=msg.split("/responding",1)[1]
-    if value.lower()=="true":
-      db["responding"]=True
-      await message.channel.send("Bot is responding")
-
-    else:
-      db["responding"]=False
-      await message.channel.send("Bot is not responding")
-
-keep_alive()
-client.run(os.getenv('TOKEN'))      
+client.run(os.getenv('TOKEN'))     
 
